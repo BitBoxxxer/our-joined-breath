000b01aefee628998117b391ff500c0c2a8e8427
Kasey <143746353+BitBoxxxer@users.noreply.github.com>
2025-08-27 18:50:47 +0500

    Initial commit

---
+++ Scripts/load_screen.gd
@@ -0,0 +1,40 @@
+extends Node2D
+
+# перенести в глобальный файл для текста.
+const INTRO_SEQUENCE = [
+	{"title": "Здравствуй", "description": ""},
+	{"title": "Предупреждения перед игрой:", "description": ""},
+	{"title": "ОСТОРОЖНО: ВСПЫШКИ СВЕТА", "description": "Эта игра содержит сцены с быстрыми вспышками света..."},
+	{"title": "ДЛЯ ЛУЧШЕГО ПОГРУЖЕНИЯ:", "description": "Рекомендуется играть ночью и в наушниках..."}
+]
+
+const TIME_TEXT_VISIBLE = 2.0
+const TIME_PAUSE_BETWEEN = 1.0
+
+@onready var MGRTL = $MG/MGRichTextLabel
+@onready var MGRTLAn = $MG/MGRichTextLabel/MG_RTLAnimationPlayer
+
+@onready var FGRTL = $FG/FGRichTextLabel
+@onready var FGRTLAn = $FG/FGRichTextLabel/FG_RTLAnimationPlayer
+
+
+func _ready():
+	start_intro_sequence()
+
+func start_intro_sequence():
+	for text_data in INTRO_SEQUENCE:
+		MGRTL.text = text_data.title
+		FGRTL.text = text_data.description
+
+		MGRTLAn.play("U")
+		FGRTLAn.play("U")
+		await (MGRTLAn.animation_finished)
+
+		await get_tree().create_timer(TIME_TEXT_VISIBLE).timeout
+
+		MGRTLAn.play("D")
+		FGRTLAn.play("D")
+		await MGRTLAn.animation_finished
+		
+		await get_tree().create_timer(TIME_PAUSE_BETWEEN).timeout
+		get_tree().change_scene_to_file("res://Scenes/Menu/3d/main_menu_3d.tscn")
