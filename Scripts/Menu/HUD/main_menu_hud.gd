34eeee7933e25cd339fb93d564751c226146b586
Kasey <koitvi4@gmail.com>
2026-03-24 13:38:05 +0500

    add debug butt for menu HUD (fast start 3d scene)

--- Scripts/Menu/HUD/main_menu_hud.gd
+++ Scripts/Menu/HUD/main_menu_hud.gd
@@ -1,6 +1,6 @@
 extends CanvasLayer
 
-@onready var textforpressd = $Control/Container/RichTextLabel
+@onready var textforpressd = $Control/Container/VBoxContainer/RichTextLabel
 @onready var mainVCBTTs = $Control/MainVCBTT
 
 signal start_butt_press
@@ -19,16 +19,24 @@ func _on_start_butt_pressed() -> void:
 
 
 func _on_options_butt_pressed() -> void:
-	pass # Replace with function body.
+	pass # Окно настроек игры, как в OWS
 
 
 func _on_achievements_butt_pressed() -> void:
-	pass # Replace with function body.
+	pass # Ачивки ? Хз, локальные очивки за нахождение всяких приколюх...
+	# Idea: Рассматривать ачивки, находить приколы даже в ачивках :D
 
 
 func _on_authors_butt_pressed() -> void:
-	pass # Replace with function body.
+	pass # Окно ссылок автора, как в OWS
 
 
 func _on_exit_butt_pressed() -> void:
-	pass # Replace with function body.
+	pass # Выход из игры - закрытие окна.
+
+
+# Тут тестовое, что нужно далее удалитб
+# TODO: Удалить код ниже в будущем
+func _on_button_quick_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Game_Map/3D/TestArea3D.tscn")
+	# потом переписать на 
