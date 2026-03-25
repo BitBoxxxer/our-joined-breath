a24ea61f96dfa019546a24573e5ee5e3a37599f2
Kasey <koitvi4@gmail.com>
2026-03-25 22:38:54 +0500

    just add a new 3d scene, где будут объекты и прочее для тестов:)

--- Scenes/HUD/Obj_Items/main_monitor.gd
+++ Scenes/HUD/Obj_Items/main_monitor.gd
@@ -11,3 +11,8 @@ func _process(delta: float) -> void:
 func _on_button_exit_pressed() -> void:
 	get_tree().change_scene_to_file("res://_NowheresTraiders/MainResourse/Scenes/shuttle.tscn")
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+func _on_button_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Game_Map/3D/TestArea3D.tscn")
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
