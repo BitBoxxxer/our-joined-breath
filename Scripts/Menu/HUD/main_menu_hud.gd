d079ea56f7577f8c696565e205777b64b92d7708
Kasey <143746353+BitBoxxxer@users.noreply.github.com>
2026-03-26 19:54:11 +0500

    just add empty windows for Menu

--- Scripts/Menu/HUD/main_menu_hud.gd
+++ Scripts/Menu/HUD/main_menu_hud.gd
@@ -12,23 +12,25 @@ func _process(_delta: float) -> void:
 	if Input.is_anything_pressed():
 		mainVCBTTs.visible = true
 		textforpressd.visible = false
-	
+
 
 func _on_start_butt_pressed() -> void:
 	emit_signal("start_butt_press")
 
 
 func _on_options_butt_pressed() -> void:
-	pass # Окно настроек игры, как в OWS
+	get_tree().change_scene_to_file("res://Scenes/HUD/Menu_Game/Option_screen.tscn")
 
 
 func _on_achievements_butt_pressed() -> void:
-	pass # Ачивки ? Хз, локальные очивки за нахождение всяких приколюх...
+	get_tree().change_scene_to_file("res://Scenes/HUD/Menu_Game/Achievement_screen.tscn")
+	# Ачивки ? Хз, локальные очивки за нахождение всяких приколюх...
 	# Idea: Рассматривать ачивки, находить приколы даже в ачивках :D
 
 
 func _on_authors_butt_pressed() -> void:
-	pass # Окно ссылок автора, как в OWS
+	get_tree().change_scene_to_file("res://Scenes/HUD/Menu_Game/Author_screen.tscn")
+	# Окно ссылок автора, как в OWS
 
 
 func _on_exit_butt_pressed() -> void:
@@ -39,4 +41,5 @@ func _on_exit_butt_pressed() -> void:
 # TODO: Удалить код ниже в будущем
 func _on_button_quick_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://_NowheresTraiders/MainResourse/Scenes/shuttle.tscn")
-	# потом переписать на 
+	# потом переписать на основное место меню.
+	# 
