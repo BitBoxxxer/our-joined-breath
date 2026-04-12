3f24d4c28d881d812e97c64b5b5f12a8d7dfb5f2
Kasey <koitvi4@gmail.com>
2026-04-12 17:22:40 +0500

    Add some stuff

--- Scenes/HUD/Obj_Items/main_monitor.gd
+++ Scenes/HUD/Obj_Items/main_monitor.gd
@@ -16,3 +16,7 @@ func _on_button_exit_pressed() -> void:
 func _on_button_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Game_Map/3D/TestArea3D.tscn")
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+func _on_button_tree_pressed() -> void:
+	pass # Replace with function body.
